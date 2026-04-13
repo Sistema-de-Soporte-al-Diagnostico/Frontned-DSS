91524efa00323a9af910394f82c1c89a13a77bc5
Sebastian Silva <u20211g296@upc.edu.pe>
2026-04-13 18:15:10 -0500

    fix: update service worker cache revision and remove peer dependencies from package-lock

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.noefqbg7qvo"
+    "revision": "0.ptt9caqvodk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
